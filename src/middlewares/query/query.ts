42c844e3fd733aa820fe4c4727ff127847c761ea
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2023-10-02 18:38:01 +0700

    feat: query for single

--- src/middlewares/query/query.ts
+++ src/middlewares/query/query.ts
@@ -1,5 +1,9 @@
 import { Handler, Request } from 'express';
-import { QueryForAllParser, ValidationError } from 'kueri';
+import {
+  QueryForAllParser,
+  QueryForSingleParser,
+  ValidationError,
+} from 'kueri';
 import { HttpError } from '../../errors/http.error';
 
 export function createQueryMiddleware(): {
@@ -30,6 +34,28 @@ export function createQueryMiddleware(): {
         return next(err);
       }
     },
-    forSingle: (req, res, next) => next(),
+    forSingle: async (req, res, next) => {
+      try {
+        const parsed = await new QueryForSingleParser().make(req.query);
+
+        req.query = parsed as unknown as Request['query'];
+
+        next();
+      } catch (err) {
+        if (err instanceof ValidationError) {
+          return next(
+            new HttpError(
+              {
+                name: 'BadRequest',
+                message: err.message,
+              },
+              400,
+            ),
+          );
+        }
+
+        return next(err);
+      }
+    },
   };
 }
