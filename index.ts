d6e986cbb0752510de5c5a6dee33d347b47e529b
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2023-10-04 06:20:40 +0700

    feat: export router group

--- index.ts
+++ index.ts
@@ -2,5 +2,7 @@ export { App } from './src/app/app';
 export type { ErrorResponse } from './src/app/app.types';
 export { HttpError } from './src/errors/http.error';
 export { Router } from './src/router/router';
+export { RouterGroup } from './src/router-group/router-group';
 export { RouterContext, RouterHandler } from './src/router/router.types';
+export { RouterGroupHandler } from './src/router-group/router-group.types';
 export * from './src/middlewares';
