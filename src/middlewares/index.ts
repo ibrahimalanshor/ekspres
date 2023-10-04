f9aeadb39ad26a71ce7826b8776a24a0c39b28b8
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2023-10-05 05:08:51 +0700

    build

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -1,1 +1,4 @@
 export { createQueryMiddleware } from './query/query';
+export { createRequestValidator } from './request-validator/request-validator';
+export { Request } from './request-validator/request';
+export { ErrorDetails } from './request-validator/error-details';
