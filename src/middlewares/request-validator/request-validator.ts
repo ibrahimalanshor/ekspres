181068ba76a4b0f8b2c9f0ca6163df451b7ffe99
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2023-10-05 05:02:22 +0700

    feat: create request validator

--- src/middlewares/request-validator/request-validator.ts
+++ src/middlewares/request-validator/request-validator.ts
@@ -1,1 +1,56 @@
-export function createRequestValidator() {}
+import { Handler } from 'express';
+import Joi from 'joi';
+import { HttpError } from '../../errors/http.error';
+import { ErrorDetails } from './error-details';
+import { Request } from './request';
+
+export function createRequestValidator(
+  RequestClass: new () => Request,
+): Handler {
+  const request = new RequestClass();
+
+  async function validateBody(
+    body: Record<string, any>,
+  ): Promise<Record<string, any>> {
+    try {
+      return await request.schema().validateAsync(body, {
+        allowUnknown: true,
+        stripUnknown: true,
+      });
+    } catch (err) {
+      if (err instanceof Joi.ValidationError) {
+        throw new ErrorDetails(err);
+      }
+
+      throw err;
+    }
+  }
+
+  return async (req, res, next) => {
+    try {
+      req.body = await validateBody(req.body);
+
+      next();
+    } catch (err) {
+      if (err instanceof ErrorDetails) {
+        return next(
+          new HttpError(
+            {
+              name: 'Unprocessable Content',
+              message: 'Validation Error',
+              details: err.errors,
+            },
+            422,
+          ),
+        );
+      }
+
+      return next(
+        new HttpError({
+          name: 'Unprocessable Content',
+          message: 'Validation Error',
+        }),
+      );
+    }
+  };
+}
