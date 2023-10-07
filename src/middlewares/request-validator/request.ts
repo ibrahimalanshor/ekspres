62e5b531c55fbab8079d5cbfbe568d32874ca78b
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2023-10-07 12:20:05 +0700

    feat: request path

--- src/middlewares/request-validator/request.ts
+++ src/middlewares/request-validator/request.ts
@@ -1,5 +1,9 @@
 import Joi from 'joi';
 
+export type RequestPath = 'query' | 'params' | 'body';
+
 export abstract class Request {
   abstract schema(): Joi.Schema;
+
+  public path: RequestPath = 'body';
 }
